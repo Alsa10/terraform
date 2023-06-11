c97e862d1ce039cb8bf3f2d3d9aaeba597248c1a
Alsa10 <alok.devops12@gmail.com>
2023-06-11 18:38:44 +0530

    frist commit in AWS Terraform

---
+++ 02-security/A-secgroup.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+
+    default = "vpc-0bb1ea46b6c4f3617"
+}
+
+
+resource "aws_security_group" "allowsg" {
+  name        = "allowsg"
+  description = "allowsge security group created with Terraform"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
